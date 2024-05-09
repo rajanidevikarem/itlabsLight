6ca6e9fe14754c52663b711d83ec8d6127d36ffd
rajanidevikarem <rajani86.k@gmail.com>
2024-05-09 23:03:03 +0530

    terms privacy styles

--- src/Views/Privacy.jsx
+++ src/Views/Privacy.jsx
@@ -14,15 +14,15 @@ const Privacy = () => {
      </div>
        </div>
        <section className='lg:py-16 py-8 mx-16'>
-    <p className='py-2 text-white px-8'>
+    <p className='py-2 text-[#16111C] px-8'>
         Our team of experienced consultants offers insightful guidance on your IT infrastructure, systems, and overall IT strategy. We can advise on cloud migration, cybersecurity solutions, business process automation, and more.
        </p>
-       <div className='flex flex-col h-full justify-center text-white' data-aos="fade-left">
+       <div className='flex flex-col h-full justify-center text-[#16111C]' data-aos="fade-left">
      
      <div className='lg:mx-8 mx-2'>
        <ul>
          <li className='flex items-start my-2'>
-         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -30,12 +30,12 @@ const Privacy = () => {
        
 </div>
              <div >
-             <p className='text-lg py-2 mx-4 my-2'>We provide the information to trusted partners who work on behalf of or with ITLABS under extremely strict confidentiality agreements. However, these companies do not have any independent right to share this information.</p>
+             <p className='py-2 mx-4 my-2'>We provide the information to trusted partners who work on behalf of or with ITLABS under extremely strict confidentiality agreements. However, these companies do not have any independent right to share this information.</p>
          
         </div>
          </li>
          <li className='flex  items-start my-2'>
-         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -43,13 +43,13 @@ const Privacy = () => {
        
 </div>
              <div >
-             <p className='text-lg py-4 mx-4 my-2'> We respond to subpoenas, court orders, or legal process, or to establish or exercise our legal rights or defend against legal claims</p>
+             <p className='py-4 mx-4 my-2'> We respond to subpoenas, court orders, or legal process, or to establish or exercise our legal rights or defend against legal claims</p>
          
        
         </div>
          </li>
          <li className='flex items-start my-2'>
-         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -57,12 +57,12 @@ const Privacy = () => {
        
 </div>
              <div >
-             <p className='text-lg py-4 mx-4 my-2'> Developing cost-effective, high-quality products and custom applications built on strict timelines using industry best practices.</p>
+             <p className='py-4 mx-4 my-2'> Developing cost-effective, high-quality products and custom applications built on strict timelines using industry best practices.</p>
          
         </div>
          </li>
          <li className='flex items-start my-2'>
-         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -70,12 +70,12 @@ const Privacy = () => {
        
 </div>
              <div >
-             <p className='text-lg py-2 mx-4 my-2'> We believe it is necessary to share information in order to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the physical safety of any person, or as otherwise required by law.</p>
+             <p className='py-2 mx-4 my-2'> We believe it is necessary to share information in order to investigate, prevent, or take action regarding illegal activities, suspected fraud, situations involving potential threats to the physical safety of any person, or as otherwise required by law.</p>
          
         </div>
          </li>
          <li className='flex items-start my-2'>
-         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+         <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -83,7 +83,7 @@ const Privacy = () => {
        
 </div>
              <div >
-             <p className='text-lg py-4 mx-4 my-2'> We transfer information about you if ITLABS is acquired by or merged with another company.</p>
+             <p className='py-4 mx-4 my-2'> We transfer information about you if ITLABS is acquired by or merged with another company.</p>
          
         </div>
          </li>
--- src/Views/Terms.jsx
+++ src/Views/Terms.jsx
@@ -14,15 +14,15 @@ const Terms = () => {
   </div>
     </div>
     <section className='lg:py-16 py-8 mx-16'>
-    <p className='py-2 text-white px-8'>
+    <p className='py-2 text-[#16111C] px-8'>
      Our team of experienced consultants offers insightful guidance on your IT infrastructure, systems, and overall IT strategy. We can advise on cloud migration, cybersecurity solutions, business process automation, and more.
     </p>
-    <div className='flex flex-col h-full justify-center text-white' data-aos="fade-left">
+    <div className='flex flex-col h-full justify-center text-[#16111C]' data-aos="fade-left">
   
   <div className='lg:mx-8 mx-2'>
     <ul>
       <li className='flex items-start my-2'>
-      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -30,12 +30,12 @@ const Terms = () => {
     
 </div>
           <div >
-          <p className='text-lg py-2 mx-4 my-2'>The term User shall refer to the user who is browsing the site. The term Niche or IT-Niche shall refer to ITLABS Incorporation. The term Site refers to www.itlabsinc.com . owned and monitored by ITLABS.</p>
+          <p className='py-2 mx-4 my-2'>The term User shall refer to the user who is browsing the site. The term Niche or IT-Niche shall refer to ITLABS Incorporation. The term Site refers to www.itlabsinc.com . owned and monitored by ITLABS.</p>
       
      </div>
       </li>
       <li className='flex  items-start my-2'>
-      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -43,13 +43,13 @@ const Terms = () => {
     
 </div>
           <div >
-          <p className='text-lg py-2 mx-4 my-2'> By using the Site, you agree to follow and be bound by the following terms and conditions concerning your use of the Site. ITLABS may revise the Terms of Use at any time without notice to you. Areas of the Site may have different terms of use posted. If there is a conflict between the Terms of Use and terms of use posted for a specific area of the Site, the latter shall have precedence with respect to your use of that area of the Site</p>
+          <p className='py-2 mx-4 my-2'> By using the Site, you agree to follow and be bound by the following terms and conditions concerning your use of the Site. ITLABS may revise the Terms of Use at any time without notice to you. Areas of the Site may have different terms of use posted. If there is a conflict between the Terms of Use and terms of use posted for a specific area of the Site, the latter shall have precedence with respect to your use of that area of the Site</p>
       
     
      </div>
       </li>
       <li className='flex items-start my-2'>
-      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -57,12 +57,12 @@ const Terms = () => {
     
 </div>
           <div >
-          <p className='text-lg py-2 mx-4 my-2'> ITLABS may terminate User's access at any time for any reason. The provisions regarding to disclaimer of warranty, accuracy of information and indemnification shall survive such termination. ITLABS may monitor access to the Site.</p>
+          <p className='py-2 mx-4 my-2'> ITLABS may terminate User's access at any time for any reason. The provisions regarding to disclaimer of warranty, accuracy of information and indemnification shall survive such termination. ITLABS may monitor access to the Site.</p>
       
      </div>
       </li>
       <li className='flex items-start my-2'>
-      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -70,12 +70,12 @@ const Terms = () => {
     
 </div>
           <div >
-          <p className='text-lg py-2 mx-4 my-2'> All content present on this site is the exclusive property of ITLABS. The software, text, images, graphics, video and audio used on this site belong to ITLABS. No material from this site may be copied, modified, reproduced, republished, uploaded, transmitted, posted or distributed in any form without prior written permission from ITLABS. All rights not expressly granted herein are reserved. Unauthorized use of the materials appearing on this site may violate copyright, trademark and other applicable laws, and could result in criminal or civil penalties. ITLABS is a registered trademark of ITLABS Incorporation. This trademark may not be used in any manner without prior written consent from ITLABS Incorporation.</p>
+          <p className='py-2 mx-4 my-2'> All content present on this site is the exclusive property of ITLABS. The software, text, images, graphics, video and audio used on this site belong to ITLABS. No material from this site may be copied, modified, reproduced, republished, uploaded, transmitted, posted or distributed in any form without prior written permission from ITLABS. All rights not expressly granted herein are reserved. Unauthorized use of the materials appearing on this site may violate copyright, trademark and other applicable laws, and could result in criminal or civil penalties. ITLABS is a registered trademark of ITLABS Incorporation. This trademark may not be used in any manner without prior written consent from ITLABS Incorporation.</p>
       
      </div>
       </li>
       <li className='flex items-start my-2'>
-      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg'>
+      <div className='py-4 px-4 items-center my-4 list-icons rounded-lg  text-white'>
        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
 </svg>
@@ -83,7 +83,7 @@ const Terms = () => {
     
 </div>
           <div >
-          <p className='text-lg py-4 mx-4 my-2'> We transfer information about you if ITLABS is acquired by or merged with another company.</p>
+          <p className='py-4 mx-4 my-2'> We transfer information about you if ITLABS is acquired by or merged with another company.</p>
       
      </div>
       </li>
